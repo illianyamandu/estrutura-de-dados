c29daa366e98bb5556ea05420650120ffdd82cdc
Illian Yamandú <yamandu.developer@gmail.com>
2023-11-04 08:14:49 -0300

    starter fila class

---
+++ fila/fila.h
@@ -0,0 +1,20 @@
+typedef int TipoItem;
+const int MAX_ITENS = 100;
+
+class fila {
+    private:
+        int primeiro, ultimo;
+        TipoItem* estrutura;
+    
+    public:
+        fila(); // construtora
+        ~fila(); // destrutora
+        bool estaVazia();
+        bool estaCheia();
+        void inserir(TipoItem item);
+        TipoItem remover();
+        void imprimir();
+        int getPrimeiro();
+        int getUltimo();
+        TipoItem getEstrutura();
+}
