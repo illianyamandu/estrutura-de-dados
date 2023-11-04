77cbb5fbf5f4cd2a8ebbe667ad7ad4465b7f8e71
Illian Yamandú <yamandu.developer@gmail.com>
2023-11-04 17:22:50 -0300

    main file of dynamic stack

---
+++ pilhaDinamica/main_pilhadinamica.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "pilhadinamica.h"
+
+using namespace std;
+
+int main() {
+    PilhaDinamica pilha1;
+    TipoItem item;
+    int opcao;
+
+    cout << "Programa gerador de pilha:\n";
+    do {
+        cout << "Digite 0 para parar o programa!\n";
+        cout << "Digite 1 para inserir um elemento!\n";
+        cout << "Digite 2 para remover um elemento!\n";
+        cout << "Digite 3 para imprimir a pilha!\n";
+
+        cin >> opcao;
+        if (opcao == 1) {
+            cout << "Digite o elemento a ser inserido na pilha: ";
+            cin >> item;
+            pilha1.inserir(item);
+        } else if (opcao == 2) {
+            item = pilha1.remover();
+            cout << "Elemento removido: " << item << endl;
+        } else if (opcao == 3) {
+            pilha1.imprimir();
+        } else if (opcao == 0) {
+            cout << "Programa encerrado!\n";
+        } else {
+            cout << "Opção invalida!\n";
+        }
+
+        cout << "\n";
+    } while (opcao > 0 && opcao <= 3);
+
+    return 0;
+}
